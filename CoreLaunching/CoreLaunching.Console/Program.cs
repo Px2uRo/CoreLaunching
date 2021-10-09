3df9f5457df270379506cd709b9c083825adc669
Howard Xiong on LEGION R7000 <hongyu233@outlook.com>
2021-10-09 11:32:56 +0800

    添加项目文件。

---
+++ CoreLaunching/CoreLaunching.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cons = System.Console;
+using cl = CoreLaunching;
+using System.IO;
+
+namespace CoreLaunching.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"..\.minecraft");
+            dir.Create();
+
+            string front = @"..\.minecraft\";
+            string verDir = @"version\";
+
+            cl.DirectorySystem dirS = new DirectorySystem();
+            dirS.DirSys(front+verDir);
+
+            cl.VersionMgr mgr = new VersionMgr();
+            mgr.LoadJson(front+verDir+@"Test.json");
+            Cons.Read();
+        }
+    }
+}
