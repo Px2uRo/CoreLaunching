069a9d0c3fbb144fdc0e83c4af7458c25e38a276
粉猫PinKcat <hongyu233@outlook.com>
2022-01-13 06:04:26 +0800

    先提交，准备干票大的！

--- CoreLaunching/CoreLaunching.Console/Program.cs
+++ CoreLaunching/CoreLaunching.Console/Program.cs
@@ -9,28 +9,23 @@ namespace CoreLaunching.Console
     {
         static void Main(string[] args)
         {
-            var tempDirPath = Environment.GetEnvironmentVariable("TEMP") + @"\CoreLaunching\";
-            (new DirectoryInfo(tempDirPath)).Create();
+            cl.Launcher launcher = new cl.Launcher();
+            cl.Launcher.JavaPath = @"""C:\Program Files\Java\jre1.8.0_291\bin\java.exe""";
+            launcher.SetMemory(256, 1536);
+            launcher.SetLauncherInfo("CoreLaunching", "0.1");
+            cl.Launcher.OtherArguments = "-XX:+UseG1GC -XX:-UseAdaptiveSizePolicy -XX:-OmitStackTraceInFastThrow -Dfml.ignoreInvalidMinecraftCertificates=True -Dfml.ignorePatchDiscrepancies=True -Dlog4j2.formatMsgNoLookups=true";
+            cl.Launcher.PlayerName = "FEN114514";
+            cl.Launcher.GameVersion = "1.17.10";
+            cl.Launcher.GameDir = @"C:\Users\Lenovo\AppData\Roaming\.minecraft\versions\1.7.10\";
+            cl.Launcher.assetIndex = "1.7";
+            cl.Launcher.uuid = "17d03005e4ae445689edf74b53bb386b";
+            cl.Launcher.accessToken = @"eyJhbGciOiJIUzI1NiJ9.eyJ4dWlkIjoiMjUzNTQ2NjEwNTMzOTE4OSIsImFnZyI6IkFkdWx0Iiwic3ViIjoiZWEwY2RkNDMtN2U1Yy00MzFkLWIxZTMtZTJkMDRiOGRhNzVmIiwibmJmIjoxNjQxOTE5NjY5LCJhdXRoIjoiWEJPWCIsInJvbGVzIjpbXSwiaXNzIjoiYXV0aGVudGljYXRpb24iLCJleHAiOjE2NDIwMDYwNjksImlhdCI6MTY0MTkxOTY2OSwicGxhdGZvcm0iOiJVTktOT1dOIiwieXVpZCI6IjUyNzczMjEzNzAyY2VkZDc3MGMzMzRlYmZhMjc0MDZlIn0.uWI-w99z_2NJ_Y3u-DLt21uJwl8KJ-EuDudIIJ93RUY";
+            cl.Launcher.userProperties = "{}";
+            cl.Launcher.userType = "Mojang";
+            
+            cl.Launcher.TargetJSON = @"C:\Users\Lenovo\AppData\Roaming\.minecraft\versions\1.7.10\1.7.10.json";
 
-            DirectoryInfo dir = new DirectoryInfo(@"..\.minecraft");
-            dir.Create();
-
-            string front = @"..\.minecraft\";
-            string verDir = @"version\";
-
-            cl.DirectorySystem dirS = new DirectorySystem();
-            dirS.DirSys(front+verDir);
-
-            cl.VersionMgr mgr = new VersionMgr();
-            try
-            {
-                mgr.LoadJson(front + verDir + @"Test.json");
-            }
-            catch
-            {
-
-            }
-            (new cl.Downloader()).NormalDownload(@"https://launchermeta.mojang.com/mc/game/version_manifest.json",tempDirPath);
+            launcher.Launch();
         }
     }
 }
---
+++ CoreLaunching/CoreLaunching/Launcher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CoreLaunching
+{
+    public class Launcher
+    {
+        public static string JavaPath { get; set; }//指定 Java 路径
+        public static string OtherArguments { get; set; }//指定额外参数
+        static string Memory;
+        /// <summary>
+        /// 分配内存
+        /// </summary>
+        /// <param name="MaxMemory">最大内存</param>
+        /// <param name="MinMemory">最小内存</param>
+        public void SetMemory(int MinMemory, int MaxMemory)
+        {
+            Memory =" " + "-Xmx" + MaxMemory.ToString() + "m" + " " + "-Xmn" + MinMemory.ToString() + "m";
+        }
+        static string LauncherInfo;
+        /// <summary>
+        /// 设置启动器信息
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Version"></param>
+        public void SetLauncherInfo(string Name,string Version)
+        {
+            LauncherInfo = " "+ "-Dminecraft.launcher.brand=" + Name +" " + "-Dminecraft.launcher.version="+Version;
+        }
+        public static string PlayerName { get; set; }
+        public static string GameVersion { get; set; }
+        public static string GameDir { get; set; }
+        public static string assetsDir { get; set; }
+        public static string assetIndex { get; set; }
+        public static string uuid { get; set; }
+        public static string accessToken { get; set; }
+        public static string userProperties { get; set; }
+        public static string userType { get; set; }
+        public static string TargetJSON { get; set; }
+        Dictionary<string, string> ELTable = new Dictionary<string, string>
+        {
+            {"${auth_player_name}", PlayerName},
+            {"${version_name}",GameVersion },
+            {"${game_directory}",GameDir },
+            {"${assets_root}",assetsDir},
+            {"${assets_index_name}",assetIndex },
+            {"${auth_uuid}",uuid },
+            {"${auth_access_token}",accessToken },
+            {"${user_properties}",userProperties },
+            {"${user_type}",userType }
+        };
+        public void Launch()
+        {
+            StreamReader loader = File.OpenText(TargetJSON);
+            JsonTextReader reader = new JsonTextReader(loader);//NewtonJson读取文件。
+            JObject jsonObject = (JObject)JToken.ReadFrom(reader);//强制转换 一个抽象的 JSON 令牌 到一个 JSON 对象。
+            var mainClass = " " + jsonObject["mainClass"]; //读取 JSON 里面的 mainClass 项目。
+            var minecraftArguments = " " + jsonObject["minecraftArguments"]; //读取 JSON 里面的 minecraftArguments 项目。
+            loader.Close();
+
+            minecraftArguments.Replace()
+
+            string FinalCommand = JavaPath + " " + OtherArguments + Memory +LauncherInfo+mainClass+minecraftArguments;
+            Console.WriteLine(FinalCommand);
+        }
+    }
+}
